bbfadeddf69ce1986c406eb5be053ccd2765e809
Eros Montin <eros.montin@gmail.com>
2025-12-06 13:15:47 -0500

    slurm

---
+++ pynico/__init__.py
@@ -0,0 +1,14 @@
+from . import pynico
+from .pynico import (
+    Node,
+    Stack,
+    Log,
+    BashIt,
+    GarbageCollector,
+    SudoGarbageCollector,
+    Timer,
+    Pathable,
+    SlurmScript
+)
+
+__version__ = "3"
